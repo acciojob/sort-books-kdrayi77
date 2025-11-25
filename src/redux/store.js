bb3b9e9377090a1e3f203e8c2d9e8613184428dd
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-11-25 20:22:59 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import booksReducer from "./booksSlice";
 
+export const store = configureStore({
+  reducer: {
+    books: booksReducer,
+  },
+});
